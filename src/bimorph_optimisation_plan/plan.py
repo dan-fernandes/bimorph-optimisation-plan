c41b503277fce997a7ee12218bfe6953a8b78b7f
Daniel Fernandes <danieljosephfernandes@gmail.com>
2023-09-01 17:02:19 +0100

    Make most of it work.

--- src/bimorph_optimisation_plan/plan.py
+++ src/bimorph_optimisation_plan/plan.py
@@ -6,6 +6,17 @@ from dodal.devices.bimorph_mirrors.CAENels_bimorph_mirror_interface import (
 )
 from dodal.devices.oav.oav_detector import OAV
 
+from ophyd import Component, Device, EpicsSignalRO
+
+
+class CentroidDevice(Device):
+    centroid_x_rbv: EpicsSignalRO = Component(
+        EpicsSignalRO, "-DI-OAV-01:STAT:CentroidX_RBV"
+    )
+    centroid_y_rbv: EpicsSignalRO = Component(
+        EpicsSignalRO, "-DI-OAV-01:STAT:CentroidY_RBV"
+    )
+
 
 def voltage_list_generator(initial_list, increment):
     yield initial_list
@@ -16,7 +27,7 @@ def voltage_list_generator(initial_list, increment):
         yield initial_list
 
 
-def slit_position_generator(
+def slit_position_generator_1d(
     slit_size, slit_centre_start, slit_centre_end, number_of_slit_positions
 ):
     slit_centre_increment = (
@@ -24,22 +35,43 @@ def slit_position_generator(
     ) / number_of_slit_positions
 
     for i in range(number_of_slit_positions):
-        yield slit_centre_increment * i, slit_size
+        yield (slit_centre_increment * i, slit_size)
 
 
-# @bpp.run_decorator
-# this wil be a stub:
-def my_plan(bimorph, target_voltages):
-    yield from bps.open_run()
+def slit_position_generator_2d(
+    x_slit_active_size,
+    x_slit_centre_start,
+    x_slit_centre_end,
+    x_number_of_slit_positions,
+    x_slit_dormant_size,
+    x_slit_dormant_centre,
+    y_slit_active_size,
+    y_slit_centre_start,
+    y_slit_centre_end,
+    y_number_of_slit_positions,
+    y_slit_dormant_size,
+    y_slit_dormant_centre,
+):
+    for position in slit_position_generator_1d(
+        x_slit_active_size,
+        x_slit_centre_start,
+        x_slit_centre_end,
+        x_number_of_slit_positions,
+    ):
+        yield (*position, y_slit_dormant_centre, y_slit_dormant_size)
+    """
+    for position in slit_position_generator_1d(
+        y_slit_active_size,
+        y_slit_centre_start,
+        y_slit_centre_end,
+        y_number_of_slit_positions,
+    ):
+        yield (x_slit_dormant_centre, x_slit_active_size, *position)
+    """
 
-    yield from bps.trigger_and_read(
-        bimorph.get_channels_by_attribute(ChannelAttribute.VOUT_RBV)
-    )
 
-    yield from bps.close_run()
-
-
-def get_centroids(
+"""
+def get_centroids_1d(
     bimorph,
     slit,
     oav,
@@ -55,13 +87,16 @@ def get_centroids(
         yield from bps.mv(bimorph, voltage_list)
         yield from bps.read(bimorph)
 
-        for slit_position in slit_position_generator(
+        for slit_position in slit_position_generator_1d(
             slit_size, slit_centre_start, slit_centre_end, number_of_slit_positions
         ):
-            yield from bps.read([slit])
             yield from bps.mv(slit, *slit_position)
+            yield from bps.read([slit])
             yield from bps.trigger_and_read([oav.stats.centroid])
+
     bps.close_run()
+"""
+
 
 def get_centroids_2d(
     bimorph,
@@ -72,37 +107,52 @@ def get_centroids_2d(
     x_slit_active_size,
     x_slit_centre_start,
     x_slit_centre_end,
-    x_slit_dormant_size,
     x_number_of_slit_positions,
+    x_slit_dormant_size,
+    x_slit_dormant_centre,
     y_slit_active_size,
     y_slit_centre_start,
     y_slit_centre_end,
-    y_slit_dormant_size,
     y_number_of_slit_positions,
+    y_slit_dormant_size,
+    y_slit_dormant_centre,
 ):
-
-    bps.open_run()
+    centroid_device = CentroidDevice(name="centroid_device", prefix=oav.prefix)
+    centroid_device.wait_for_connection()
+    yield from bps.open_run()
     for voltage_list in voltage_list_generator(initial_voltage_list, voltage_increment):
         yield from bps.mv(bimorph, voltage_list)
-        yield from bps.read(bimorph)
 
-        for slit_position in slit_position_generator(
-            x_slit_active_size, x_slit_centre_start, x_slit_centre_end, x_number_of_slit_positions
+        for slit_position in slit_position_generator_2d(
+            x_slit_active_size,
+            x_slit_centre_start,
+            x_slit_centre_end,
+            x_number_of_slit_positions,
+            x_slit_dormant_size,
+            x_slit_dormant_centre,
+            y_slit_active_size,
+            y_slit_centre_start,
+            y_slit_centre_end,
+            y_number_of_slit_positions,
+            y_slit_dormant_size,
+            y_slit_dormant_centre,
         ):
-            yield from bps.read([slit])
-            yield from bps.mv(slit, *slit_position)
-            yield from bps.trigger_and_read([oav.stats.centroid])
-        
-        for slit_position in slit_position_generator(
-            y_slit_active_size, y_slit_centre_start, y_slit_centre_end, y_number_of_slit_positions
-        ):
-            
-        
-    bps.close_run()
+            yield from bps.mv(slit, slit_position)
 
-def oav_plan(oav: OAV):
-    yield from bps.open_run()
+            yield from bps.create()
 
-    yield from bps.trigger_and_read([oav.stats.centroid])
+            for signal in bimorph.get_channels_by_attribute(ChannelAttribute.VOUT_RBV):
+                yield from bps.read(signal)
+
+            for signal in (slit.x_size, slit.x_centre, slit.y_size, slit.y_centre):
+                yield from bps.read(signal)
+
+            yield from bps.stage(oav)
+            yield from bps.trigger(oav)
+            # yield from bps.read(oav.stats)
+            yield from bps.read(centroid_device)
+            yield from bps.unstage(oav)
+
+            yield from bps.save()
 
     yield from bps.close_run()
--- tests/test_plan.py
+++ tests/test_plan.py
@@ -5,35 +5,69 @@ from bluesky.callbacks import LiveTable
 
 # from bluesky.callbacks.best_effort import BestEffortCallback
 
-from dodal.devices.bimorph_mirrors.CAENels_bimorph_mirror_8_channel import (
-    CAENelsBimorphMirror8Channel,
+from dodal.devices.bimorph_mirrors.CAENels_bimorph_mirror_7_channel import (
+    CAENelsBimorphMirror7Channel,
 )
 from dodal.devices.oav.oav_detector import OAV
-from dodal.devices.slits.S5_BL02J_AL_SLITS_95 import S5_BL02J_AL_SLITS_95 as Slit
+from dodal.devices.slits.S5_BL02J_AL_SLITS_95 import S5_BL02J_AL_SLITS_95
+from dodal.devices.slits.I24_SLITS_04_VIRTUAL_MOTORS import (
+    I24_SLITS_04_VIRTUAL_MOTORS as Slit,
+)
 
-from bimorph_optimisation_plan.plan import get_centroids_1d
+from bimorph_optimisation_plan.plan import get_centroids_2d
 
 RE = RunEngine({})
 # bec = BestEffortCallback()
 # RE.subscribe(bec)
-BIMORPH_PREFIX = "BL02J-EA-IOC-97:G0:"
-SLIT_PREFIX = "BL02J-AL-SLITS-95:"
+# BIMORPH_PREFIX = "BL02J-EA-IOC-97:G0:"
+BIMORPH_PREFIX = "BL24I-OP-PFM-01:G1:"
+# SLIT_PREFIX = "BL02J-AL-SLITS-95:"
+SLIT_PREFIX = "BL24I-AL-SLITS-02:"
 OAV_PREFIX = "BL24I"
+"""
+"initial_voltage_list": [
+    -118.0,
+    -125.0,
+    -179.0,
+    -202.0,
+    -194.0,
+    -242.0,
+    -227.0,
+    -195.0,
+    -231.0,
+    -282.0,
+    -172.0,
+    -188.0,
+    -137.0,
+    -51.0,
+    1.0,
+    -69.0,
+],
+"""
 
 CONFIG = {
-    "initial_voltage_list": [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    "initial_voltage_list": [-145.0, -82.0, -76.0, -29.0 - 3.0, 54.0, 121.0],
     "voltage_increment": 200,
-    "slit_size": 1,
-    "slit_centre_start": 0,
-    "slit_centre_end": 10,
-    "number_of_slit_positions": 10,
+    "x_slit_size": 0.025,
+    "x_slit_centre_start": -11.5,
+    "x_slit_centre_end": 24.5,
+    "x_number_of_slit_positions": 240,  # 180,
+    "x_slit_dormant_centre": -9.78,
+    "x_slit_dormant_size": 3.0,
+    "y_slit_size": 0.02,
+    "y_slit_centre_start": 8.6,
+    "y_slit_centre_end": 10.0,
+    "y_number_of_slit_positions": 60,  # 240,
+    "y_slit_dormant_centre": 2.33,
+    "y_slit_dormant_size": 3.0,
     "camera_exposure": 1,
-    "bimorph_settle_time": 600,
+    "bimorph_settle_time": 30,
+    "output_file_path": "/home/fiw35684/temp/bimorph_test_output.csv",
 }
 
 
 def get_bimorph(bimorph_prefix=BIMORPH_PREFIX):
-    bimorph = CAENelsBimorphMirror8Channel(name="bimorph", prefix=bimorph_prefix)
+    bimorph = CAENelsBimorphMirror7Channel(name="bimorph", prefix=bimorph_prefix)
     bimorph.wait_for_connection()
     return bimorph
 
@@ -50,22 +84,103 @@ def get_oav(oav_prefix=OAV_PREFIX):
     return oav
 
 
-def test_get_centroids_1d(config=CONFIG):
+def make_csv(docs):
+    csv_dict = {}
+    headers = []
+
+    for doc in docs:
+        if "run_start" in doc and "data_keys" in doc:
+            headers = [header for header in doc["data_keys"]]
+            headers.sort()
+            for header in headers:
+                csv_dict[header] = []
+
+        elif "descriptor" in doc:
+            for header in doc["data"]:
+                csv_dict[header].append(str(doc["data"][header]))
+
+    csv_str = ",".join(headers) + "\n"
+
+    # print([row for row in zip(*[csv_dict[header] for header in headers])])
+
+    csv_str += "\n".join(
+        [",".join(row) for row in zip(*[csv_dict[header] for header in headers])]
+    )
+
+    # return csv_str
+    return csv_str
+
+
+def test_get_centroids_2d(config=CONFIG):
     bimorph = get_bimorph()
+    bimorph.settle_time = config["bimorph_settle_time"]
     slit = get_slit()
     oav = get_oav()
+    import json
+
+    def dump_docs(_, doc):
+        breakpoint()
+        print(json.dumps(doc))
+        print("\nfoo\n")
+
+    my_list = []
+
+    def aggregate_docs(_, doc):
+        my_list.append(doc)
 
     RE(
-        get_centroids_1d(
+        get_centroids_2d(
             bimorph,
             slit,
             oav,
             config["initial_voltage_list"],
             config["voltage_increment"],
-            config["slit_size"],
-            config["slit_centre_start"],
-            config["slit_centre_end"],
-            config["number_of_slit_positions"],
+            config["x_slit_size"],
+            config["x_slit_centre_start"],
+            config["x_slit_centre_end"],
+            config["x_number_of_slit_positions"],
+            config["x_slit_dormant_size"],
+            config["x_slit_dormant_centre"],
+            config["y_slit_size"],
+            config["y_slit_centre_start"],
+            config["y_slit_centre_end"],
+            config["y_number_of_slit_positions"],
+            config["y_slit_dormant_size"],
+            config["y_slit_dormant_centre"],
         ),
-        print,
+        aggregate_docs,
     )
+
+    csv = make_csv(my_list)
+    print(csv)
+    with open(config["output_file_path"], "w") as file:
+        file.write(csv)
+
+
+def test_reads():
+    bimorph = get_bimorph()
+    slit = get_slit()
+    import json
+
+    def dump_docs(_, doc):
+        print(json.dumps(doc))
+
+    import bluesky.plan_stubs as bps
+    from dodal.devices.bimorph_mirrors.CAENels_bimorph_mirror_interface import (
+        ChannelAttribute,
+    )
+
+    def read_bimorph_slit_plan(bimorph, slit):
+        bps.open_run()
+        bps.create()
+        for signal in bimorph.get_channels_by_attribute(ChannelAttribute.VOUT_RBV):
+            a = yield from bps.read(signal)
+            print(a)
+
+        for signal in (slit.x_size, slit.x_centre, slit.y_size, slit.y_centre):
+            a = yield from bps.read(signal)
+            print(a)
+        bps.save()
+        bps.close_run()
+
+    RE(read_bimorph_slit_plan(bimorph, slit), dump_docs)
