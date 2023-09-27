5735aca91b11f3d6ee484a4854f641d39f9dde1c
Daniel Fernandes <danieljosephfernandes@gmail.com>
2023-09-27 15:23:49 +0100

    Made get_centroids_2d take reading from both axes with two different OAVs

--- src/bimorph_optimisation_plan/plan.py
+++ src/bimorph_optimisation_plan/plan.py
@@ -8,6 +8,8 @@ from dodal.devices.oav.oav_detector import OAV
 
 from ophyd import Component, Device, EpicsSignalRO
 
+from enum import Enum
+
 
 class CentroidDevice(Device):
     centroid_x_rbv: EpicsSignalRO = Component(
@@ -37,7 +39,7 @@ def slit_position_generator_1d(
     for i in range(number_of_slit_positions):
         yield (slit_centre_increment * i, slit_size)
 
-
+"""
 def slit_position_generator_2d(
     x_slit_active_size,
     x_slit_centre_start,
@@ -59,7 +61,7 @@ def slit_position_generator_2d(
         x_number_of_slit_positions,
     ):
         yield (*position, y_slit_dormant_centre, y_slit_dormant_size)
-    """
+
     for position in slit_position_generator_1d(
         y_slit_active_size,
         y_slit_centre_start,
@@ -67,7 +69,7 @@ def slit_position_generator_2d(
         y_number_of_slit_positions,
     ):
         yield (x_slit_dormant_centre, x_slit_active_size, *position)
-    """
+"""
 
 
 """
@@ -101,7 +103,8 @@ def get_centroids_1d(
 def get_centroids_2d(
     bimorph,
     slit,
-    oav,
+    x_oav,
+    y_oav,
     initial_voltage_list,
     voltage_increment,
     x_slit_active_size,
@@ -117,42 +120,46 @@ def get_centroids_2d(
     y_slit_dormant_size,
     y_slit_dormant_centre,
 ):
-    centroid_device = CentroidDevice(name="centroid_device", prefix=oav.prefix)
-    centroid_device.wait_for_connection()
+
+    def take_readings(oav):
+        centroid_device = CentroidDevice(name=f"{oav.prefix}_centroid_device", prefix=oav.prefix)
+        yield from bps.create()
+
+        for signal in bimorph.get_channels_by_attribute(ChannelAttribute.VOUT_RBV):
+            yield from bps.read(signal)
+
+        for signal in (slit.x_size, slit.x_centre, slit.y_size, slit.y_centre):
+            yield from bps.read(signal)
+
+        yield from bps.stage(oav)
+        yield from bps.trigger(oav)
+        # yield from bps.read(oav.stats)
+        yield from bps.read(centroid_device)
+        yield from bps.unstage(oav)
+
+        yield from bps.save()
+
+
     yield from bps.open_run()
+
     for voltage_list in voltage_list_generator(initial_voltage_list, voltage_increment):
         yield from bps.mv(bimorph, voltage_list)
 
-        for slit_position in slit_position_generator_2d(
-            x_slit_active_size,
-            x_slit_centre_start,
-            x_slit_centre_end,
-            x_number_of_slit_positions,
-            x_slit_dormant_size,
-            x_slit_dormant_centre,
-            y_slit_active_size,
-            y_slit_centre_start,
-            y_slit_centre_end,
-            y_number_of_slit_positions,
-            y_slit_dormant_size,
-            y_slit_dormant_centre,
+        for x_position in slit_position_generator_1d(
+            x_slit_active_size, x_slit_centre_start, x_slit_centre_end, x_number_of_slit_positions
         ):
+            slit_position = (*x_position, y_slit_dormant_centre, y_slit_dormant_size)
             yield from bps.mv(slit, slit_position)
 
-            yield from bps.create()
+            take_readings(x_oav)
 
-            for signal in bimorph.get_channels_by_attribute(ChannelAttribute.VOUT_RBV):
-                yield from bps.read(signal)
+       
+        for y_position in slit_position_generator_1d(
+            y_slit_active_size, y_slit_centre_start, y_slit_centre_end, y_number_of_slit_positions
+        ):
+            slit_position = (x_slit_dormant_centre, x_slit_dormant_size, *y_position)
+            yield from bps.mv(slit, slit_position)
 
-            for signal in (slit.x_size, slit.x_centre, slit.y_size, slit.y_centre):
-                yield from bps.read(signal)
-
-            yield from bps.stage(oav)
-            yield from bps.trigger(oav)
-            # yield from bps.read(oav.stats)
-            yield from bps.read(centroid_device)
-            yield from bps.unstage(oav)
-
-            yield from bps.save()
+            take_readings(y_oav)
 
     yield from bps.close_run()
