d6fdcd144175d63be2f3c2fc545c9f42e8803f3c
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-05-16 11:32:36 +0100

    Add make_csv, define_data_aggregator

--- src/bimorph_optimisation_plan/data_saver.py
+++ src/bimorph_optimisation_plan/data_saver.py
@@ -27,4 +27,63 @@ def generate_filename(file_prefix:str = None, file_timestamp_format:str = None):
     return filename
 
 
+def make_csv(docs):
+    """
+    Takes a list of RunEngine output docs and converts to CSV format
+
+    Args:
+        docs: A list of RunEngine docs
+    
+    Returms:
+        A string of given list's csv equivalent
+    """
+    csv_dict = {}
+    headers = []
+
+    for doc in docs:
+        if "run_start" in doc and "data_keys" in doc:
+            headers = [header for header in doc["data_keys"]]
+            headers.sort()
+            for header in headers:
+                csv_dict[header] = []
+
+        elif "descriptor" in doc:
+            for header in doc["data"]:
+                csv_dict[header].append(str(doc["data"][header]))
+
+    csv_str = ",".join(headers) + "\n"
+
+    csv_str += "\n".join(
+        [",".join(row) for row in zip(*[csv_dict[header] for header in headers])]
+    )
+
+    # return csv_str
+    return csv_str
+
+
+def define_data_aggregator(filepath: str, filename: str):
+    """
+    Create a data structure and defines a function to give to Run Engine to save data
+
+    Args:
+        filepath: Directory to save file into
+        filename: Filename for output csv
+    
+    Returns:
+        A list that aggregates data, a function to give as sub to Run Engine
+    """
+
+    data_list = []
+
+    def aggregate_docs(_, doc):
+        data_list.append(doc)
+        csv = make_csv(data_list)
+
+        with open(filepath + '/' + filename, 'w') as file:
+            file.write(csv)
+    
+    return data_list, aggregate_docs
+
+
+
 
