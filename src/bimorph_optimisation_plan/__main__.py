dd1b4e135768b65917e742eda8760f59735ed2e9
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-05-16 10:41:25 +0100

    Add run_plan, instantiate devices

--- src/bimorph_optimisation_plan/__main__.py
+++ src/bimorph_optimisation_plan/__main__.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser
+from bluesky import RunEngine
+import device_instantiator
 import json
 
 from . import __version__
@@ -15,6 +17,30 @@ def main(args=None):
     with open(args.config_filepath[0]) as file:
         config_dict = json.load(file)
     
+
+def run_plan(config_dict):
+    RE = RunEngine({})
+
+    bimorph = device_instantiator.get_bimorph(
+        config.get("bimorph_type"),
+        config.get("bimorph_prefix"),
+        config.get("bimorph_name")
+    )
+
+    slit = device_instantiator.get_slit(
+        config.get("slit_type"),
+        config.get("slit_prefix"),
+        config.get("slit_name")
+    )
+
+    oav = device_instantiator.get_oav(
+        config.get("oav_zoom_parameters_filepath"),
+        config.get("oav_display_configuration_filepath"),
+        config.get("oav_prefix"),
+        config.get("oav_name")
+    )
+
+
 
 # test with: python -m bimorph_optimisation_plan
 if __name__ == "__main__":
