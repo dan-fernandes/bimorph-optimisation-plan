24de13b012ba83165bcd85d40d2df038c7166a10
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-05-16 10:40:42 +0100

    Add config parse to main

--- src/bimorph_optimisation_plan/__main__.py
+++ src/bimorph_optimisation_plan/__main__.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+import json
 
 from . import __version__
 
@@ -7,9 +8,13 @@ __all__ = ["main"]
 
 def main(args=None):
     parser = ArgumentParser()
+    parser.add_argument("config_filepath", type=str, nargs=1, help="Filepath to configuration json")
     parser.add_argument("-v", "--version", action="version", version=__version__)
     args = parser.parse_args(args)
 
+    with open(args.config_filepath[0]) as file:
+        config_dict = json.load(file)
+    
 
 # test with: python -m bimorph_optimisation_plan
 if __name__ == "__main__":
