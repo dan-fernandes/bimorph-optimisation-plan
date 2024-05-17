96e691045796d47d20c23484afbf9f815df5bc78
Daniel Fernandes <danieljosephfernandes@gmail.com>
2024-05-17 15:57:34 +0100

    Change test config path

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,9 @@
 import pytest
-
 from bimorph_optimisation_plan.__main__ import run_plan
 
+
 def test_run_plan():
-    with open("test_config.json") as file:
-            config_dict = json.load(file)
-    
+    with open("./tests/test_config.json") as file:
+        config_dict = json.load(file)
+
     run_plan(config_dict)
